3f4f74adb8631140494b91e0ee9cb14b0584e04e
Vishal <vmodak@enova.com>
2019-01-22 14:38:08 -0600

    Added PaymentListProducerWorker to svc-payment

--- svc-payment/app/workers/payment_producer_worker.rb
+++ svc-payment/app/workers/payment_producer_worker.rb
@@ -8,23 +8,5 @@ class PaymentProducerWorker < ApplicationWorker
       sourceAccountNumber: "8601860", sourceObligationNumber: "11544267", sourcePaymentNumber: "2012323")
     Payment::PaymentResponder.call(payment)
 
-    # payments = Payments_by_loanNumber["2015CA169772974"]
-    # data = JSON.parse(payments)
-    # paymentList = Avro::PaymentList.new
-    # data['payments'].map do |payment|
-    #   # puts payment['loanNumber'] + "/" + payment['sourcePaymentNumber']
-    #
-    #   paymentList.payments << Avro::Payment.new(
-    #       paid: payment['paid'],
-    #       datePaid: !payment['datePaid'].nil? ? payment['datePaid'] : "",
-    #       amountInCents: payment['amountInCents'].to_i,
-    #       loanNumber: payment['loanNumber'],
-    #       sourceAccountNumber: payment['sourceAccountNumber'],
-    #       sourcePaymentNumber: payment['sourcePaymentNumber'],
-    #       sourceObligationNumber: payment['sourceObligationNumber']
-    #   )
-    # end
-    #
-    # Payment::PaymentResponder.call(paymentList)
   end
 end
